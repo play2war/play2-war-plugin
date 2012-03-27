e6dfb82bab26470a702953f82111dc1f99780de2
Damien Lecan <dev@dlecan.com>
2012-03-27 14:04:23 +0200

    WIP on commands (doesn't compile)

---
+++ src/main/scala/plugin/Play2WarCommands.scala
@@ -0,0 +1,104 @@
+package sbt
+
+import Keys._
+import CommandSupport.{ ClearOnFailure, FailureWall }
+import complete.Parser
+import Parser._
+import Cache.seqFormat
+import sbinary.DefaultProtocol.StringFormat
+import play.api._
+import play.core._
+import play.utils.Colors
+import PlayKeys._
+import scala.annotation.tailrec
+import scala.collection.JavaConverters._
+import java.lang.{ ProcessBuilder => JProcessBuilder }
+import java.util.jar.Manifest
+import java.io._
+
+trait Play2WarCommands {
+
+  val war = TaskKey[File]("war", "Build the standalone application package as a WAR file")
+  val warTask = (baseDirectory, playPackageEverything, dependencyClasspath in Runtime, target, normalizedName, version) map { (root, packaged, dependencies, target, id, version) =>
+
+    import sbt.NameFilter._
+
+    val warDir = root / "war"
+    val packageName = id + "-" + version
+    val war = warDir / (packageName + ".war")
+
+    IO.createDirectory(warDir)
+
+    // TODO : filter *servlet-api*.jar
+    val libs = {
+      dependencies.filterNot(_.data.name.contains("servlet")).filter(_.data.ext == "jar").map { dependency =>
+        dependency.data -> ("WEB-INF/lib/" + (dependency.metadata.get(AttributeKey[ModuleID]("module-id")).map { module =>
+          module.organization + "." + module.name + "-" + module.revision + ".jar"
+        }.getOrElse(dependency.data.getName)))
+      } ++ packaged.map(jar => jar -> ("WEB-INF/lib/" + jar.getName))
+    }
+
+    val webxml = warDir / "web.xml"
+    
+    if (!webxml.exists) {    
+    	IO.write(webxml,
+    			"""<?xml version="1.0" ?>
+<web-app xmlns="http://java.sun.com/xml/ns/j2ee"
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+        xsi:schemaLocation="http://java.sun.com/xml/ns/javaee http://java.sun.com/xml/ns/javaee/web-app_3_0.xsd"
+        version="3.0">
+  
+  <display-name>Play! (%APPLICATION_NAME%)</display-name>
+  
+  <!--context-param>
+    <param-name>play.id</param-name>
+    <param-value>%PLAY_ID%</param-value>
+  </context-param>
+  
+  <listener>
+      <listener-class>play.core.server.servlet.PlayServletWrapper</listener-class>
+  </listener>
+  
+  <servlet>
+    <servlet-name>play</servlet-name>
+    <servlet-class>play.core.server.servlet.PlayServletWrapper</servlet-class>	
+  </servlet>
+    	    
+  <servlet-mapping>
+    <servlet-name>play</servlet-name>
+    <url-pattern>/</url-pattern>
+  </servlet-mapping-->
+
+</web-app>
+""" /* */ )
+    }
+
+	val webxmlFile = Seq(webxml -> ("WEB-INF/web.xml"))
+    
+    val config = Option(System.getProperty("config.file"))
+
+    val productionConfig = target / "application.conf"
+
+    val prodApplicationConf = config.map { location =>
+
+      IO.copyFile(new File(location), productionConfig)
+      Seq(productionConfig -> ("WEB-INF/classes/application.conf"))
+    }.getOrElse(Nil)
+
+    val manifest = new Manifest(
+        new ByteArrayInputStream((
+          "Manifest-Version: 1.0\n"
+          ).getBytes
+        )
+     )
+    
+    IO.jar(libs ++ prodApplicationConf ++ webxmlFile, war, manifest)
+    IO.delete(productionConfig)
+
+    println()
+    println("Your application is ready in " + war.getCanonicalPath)
+    println()
+
+    war
+  }
+}
--- src/main/scala/plugin/Play2WarPlugin.scala
+++ src/main/scala/plugin/Play2WarPlugin.scala
@@ -1,7 +1,15 @@
 package com.github.play2.warplugin
 
 import sbt._
+import Keys._
 
 object Play2WarPlugin extends Plugin {
+	
+  override lazy val settings = Seq(commands += myCommand)
 
+  lazy val myCommand =
+    Command.command("hello") { (state: State) =>
+      println("Hi!")
+      state
+    }
 }
