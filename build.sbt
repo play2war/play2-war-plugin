db14c145e341ac45d3fca7f7d6eac5c261b1bc61
Damien Lecan <dev@dlecan.com>
2012-03-29 16:53:09 +0200

    Split plugin into 2 modules : plugin and core

--- build.sbt
+++ build.sbt
@@ -1,23 +1,7 @@
-sbtPlugin := true
-
-organization := "com.github.play2.warplugin"
-
-name := "play2-war-plugin"
-
 resolvers ++= Seq(
     DefaultMavenRepository,
     "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
 )
-
-libraryDependencies ++= Seq(
-  "play"			%% "play"          % "2.0"
-)
-
-libraryDependencies <++= (scalaVersion, sbtVersion) { (scalaVersion, sbtVersion) => 
-	Seq(
-		"play"			% "sbt-plugin"    % "2.0" extra("scalaVersion" -> scalaVersion, "sbtVersion" -> sbtVersion)
-	)
-}
 
 // Download sources when executing "eclipse" command
 EclipseKeys.withSource := true
---
+++ project/Build.scala
@@ -0,0 +1,39 @@
+import sbt._
+import Keys._
+
+object Build extends Build {
+
+    lazy val root = Project(id = "play2-war",
+								base = file("."),
+								settings = commonSettings ++ Seq(
+									publishArtifact := false  
+									)
+							) aggregate(play2WarCore, play2WarPlugin)
+
+    lazy val play2WarCore = Project(id = "play2-war-core",
+								base = file("core"),
+								settings = commonSettings ++ Seq(
+									sbtPlugin := false,
+									libraryDependencies ++= Seq("play"			%% "play"          % "2.0")
+								)
+						   )
+
+    lazy val play2WarPlugin = Project(id = "play2-war-plugin",
+								base = file("plugin"),
+								settings = commonSettings ++ Seq(
+									sbtPlugin := true,
+									libraryDependencies <++= (scalaVersion, sbtVersion) { (scalaVersion, sbtVersion) => 
+										Seq(
+											"play"			% "sbt-plugin"    % "2.0" extra("scalaVersion" -> scalaVersion, "sbtVersion" -> sbtVersion)
+										)
+									}
+								)
+							) dependsOn(play2WarCore)
+
+def commonSettings = Defaults.defaultSettings ++
+    Seq(
+      organization := "com.github.play2.warplugin",
+      // version is defined in version.sbt in order to support sbt-release
+      scalacOptions ++= Seq("-unchecked", "-deprecation"),
+    )						   
+}
