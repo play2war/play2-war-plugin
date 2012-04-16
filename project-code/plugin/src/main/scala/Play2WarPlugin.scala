4fba19964c471ca3675eda401f9878955823eae7
Damien Lecan <dev@dlecan.com>
2012-04-16 23:44:37 +0200

    Clean-up

--- project-code/plugin/src/main/scala/Play2WarPlugin.scala
+++ project-code/plugin/src/main/scala/Play2WarPlugin.scala
@@ -6,10 +6,5 @@ import Keys._
 object Play2WarPlugin extends Plugin with Play2WarCommands with Play2WarSettings {
 	
   override lazy val settings = defaultSettings
-//
-//  lazy val myCommand =
-//    Command.command("hello") { (state: State) =>
-//      println("Hi!")
-//      state
-//    }
+  
 }
