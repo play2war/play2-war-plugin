178d9953428af3fc9681d5a0e10c1113822b9399
Damien Lecan <dev@dlecan.com>
2012-04-23 17:37:48 +0200

    Set cookies in this controller

--- sample/app/controllers/Application.scala
+++ sample/app/controllers/Application.scala
@@ -9,4 +9,12 @@ object Application extends Controller {
     Ok(views.html.index("Your new application is ready."))
   }
   
+  def setCookies = Action {
+    val result = Ok(views.html.index("Cookies have been set."))
+    
+    val cookie1 = new Cookie("cookie1", "value1", 3600)
+    val cookie2 = new Cookie("cookie2", "value2", 3600)
+    
+    result.withCookies(cookie1).withCookies(cookie2)
+  }
 }
--- sample/project/plugins.sbt
+++ sample/project/plugins.sbt
@@ -8,4 +8,6 @@ resolvers += "Play2war plugins snapshot" at "http://repository-play-war.forge.cl
 // Use the Play sbt plugin for Play projects
 addSbtPlugin("play" % "sbt-plugin" % "2.0")
 
+addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "2.0.0")
+
 addSbtPlugin("com.github.play2war" % "play2-war-plugin" % "0.3-SNAPSHOT")
