240b71a99bdc78b0ca9729d3f29b837ec927d42f
Damien Lecan <dev@dlecan.com>
2012-04-23 18:37:09 +0200

    WIP on getCookie

--- sample/app/controllers/Application.scala
+++ sample/app/controllers/Application.scala
@@ -4,17 +4,35 @@ import play.api._
 import play.api.mvc._
 
 object Application extends Controller {
-  
+
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
-  
+
   def setCookies = Action {
     val result = Ok(views.html.index("Cookies have been set."))
-    
+
     val cookie1 = new Cookie("cookie1", "value1", 3600)
     val cookie2 = new Cookie("cookie2", "value2", 3600)
-    
+
     result.withCookies(cookie1).withCookies(cookie2)
+  }
+
+  def getCookies = Action { request =>
+    val listCookies: List[Cookie] = List.empty
+
+    request.cookies.get("cookies1").map { cookie =>
+      listCookies ++ Seq(cookie)
+    }
+
+    request.cookies.get("cookies2").map { cookie =>
+      listCookies ++ Seq(cookie)
+    }
+
+    val mapCookies: Map[String, String] = listCookies.map {
+      c => (c.name, c.value)
+    }.toMap
+
+    Ok(views.html.getCookies(mapCookies))
   }
 }
