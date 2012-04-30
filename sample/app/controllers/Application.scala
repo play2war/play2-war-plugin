fa363ce737ac6d8cea505a8f3867c0d7bd189359
Damien Lecan <dev@dlecan.com>
2012-04-30 14:48:12 +0200

    Formating

--- sample/app/controllers/Application.scala
+++ sample/app/controllers/Application.scala
@@ -20,9 +20,9 @@ object Application extends Controller {
 
   def getCookies = Action { request =>
     var listCookies = List[Cookie]()
-    
+
     request.cookies.get("cookie1").map { cookie =>
-        listCookies ++= Seq(cookie)
+      listCookies ++= Seq(cookie)
     }
 
     request.cookies.get("cookie2").map { cookie =>
@@ -32,7 +32,7 @@ object Application extends Controller {
     val mapCookies: Map[String, String] = listCookies.map {
       c => (c.name, c.value)
     }.toMap
-    
+
     println("Cookies sent to view:" + mapCookies)
 
     Ok(views.html.getCookies(mapCookies))
@@ -41,12 +41,12 @@ object Application extends Controller {
   def redirectLanding = Action {
     Ok(views.html.redirectLanding())
   }
-  
+
   def redirect = Action {
-    Redirect(routes.Application.redirectLanding) 
+    Redirect(routes.Application.redirectLanding)
   }
-  
-  def internalServerError = Action {  request =>
+
+  def internalServerError = Action { request =>
     throw new RuntimeException("This a desired exception in order to test exception interception")
   }
 }
