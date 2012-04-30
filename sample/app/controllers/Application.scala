ead70548ae85c95b30eecaf30543f61ebad3e006
Damien Lecan <dev@dlecan.com>
2012-04-30 13:49:13 +0200

    Formating

--- sample/app/controllers/Application.scala
+++ sample/app/controllers/Application.scala
@@ -23,9 +23,9 @@ object Application extends Controller {
 
   def getCookies = Action { request =>
     var listCookies = List[Cookie]()
-    
+
     request.cookies.get("cookie1").map { cookie =>
-        listCookies ++= Seq(cookie)
+      listCookies ++= Seq(cookie)
     }
 
     request.cookies.get("cookie2").map { cookie =>
@@ -35,7 +35,7 @@ object Application extends Controller {
     val mapCookies: Map[String, String] = listCookies.map {
       c => (c.name, c.value)
     }.toMap
-    
+
     println("Cookies sent to view:" + mapCookies)
 
     Ok(views.html.getCookies(mapCookies))
@@ -44,31 +44,30 @@ object Application extends Controller {
   def redirectLanding = Action {
     Ok(views.html.redirectLanding())
   }
-  
+
   def redirect = Action {
-    Redirect(routes.Application.redirectLanding) 
+    Redirect(routes.Application.redirectLanding)
   }
-  
-  def internalServerError = Action {  request =>
+
+  def internalServerError = Action { request =>
     throw new RuntimeException("This a desired exception in order to test exception interception")
   }
-  
+
   def bigContent = Action {
-    
+
     val data = new Array[Byte](2 * 1024 * 1024)
     val dataContent: Enumerator[Array[Byte]] = Enumerator.fromStream(new ByteArrayInputStream(data))
-    
+
     SimpleResult(
       header = ResponseHeader(200, Map(CONTENT_LENGTH -> data.length.toString)),
-      body = dataContent
-    )
+      body = dataContent)
   }
-  
+
   def chunkedBigContent = Action {
-    
+
     val data = new Array[Byte](2 * 1024 * 1024)
     val dataContent: Enumerator[Array[Byte]] = Enumerator.fromStream(new ByteArrayInputStream(data))
-    
+
     Ok.stream(dataContent)
   }
 }
