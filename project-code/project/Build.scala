eac475bb6a7bfb09567e495ebb646ef92c149878
Damien Lecan <dev@dlecan.com>
2012-05-10 13:52:39 +0200

    Local publish because SBT doesn't handle correctly Webdav repositories

--- project-code/project/Build.scala
+++ project-code/project/Build.scala
@@ -54,11 +54,12 @@ object Build extends Build {
       // version is defined in version.sbt in order to support sbt-release
       scalacOptions ++= Seq("-unchecked", "-deprecation"),
       resolvers += ("Typsafe releases" at "http://repo.typesafe.com/typesafe/releases/"),
-	  publishTo <<= (version) {
-		version: String =>
-		  if (version.trim.endsWith("SNAPSHOT")) Some("snapshot" at cloudbees + "snapshot/")
-		  else                                   Some("release"  at cloudbees + "release/")
-	  },
+      publishTo := Some(Resolver.file("file",  file(Path.userHome.absolutePath + "/.ivy2/publish")) ),
+//    publishTo <<= (version) {
+//		version: String =>
+//		  if (version.trim.endsWith("SNAPSHOT")) Some("snapshot" at cloudbees + "snapshot/")
+//		  else                                   Some("release"  at cloudbees + "release/")
+//	  },
 //      credentials += Credentials(file("/private/play-war/.credentials")),
 //      credentials += Credentials(file(Path.userHome.absolutePath + "/.ivy2/.credentials")),
       publishMavenStyle := true,
