990f9195f785059ad42c11368ec1a56f5f65f2eb
Damien Lecan <dev@dlecan.com>
2012-05-31 17:50:29 +0200

    Disable loading of iframe demo because it corrupts integration tests

---
+++ project-code/integration-tests/src/test/scala/Helpers.scala
@@ -0,0 +1,27 @@
+
+import java.net._
+import com.gargoylesoftware.htmlunit._
+import com.gargoylesoftware.htmlunit.util._
+import scala.collection.JavaConverters._
+
+class SkipClockiFrameWrapper(val webClient: WebClient) extends WebConnectionWrapper(webClient) {
+
+  val PATTERN_TO_SKIP = "clock"
+
+  override def getResponse(webRequest: WebRequest): WebResponse = {
+    val url = webRequest.getUrl
+    val path = url.getPath()
+
+    if (path.contains(PATTERN_TO_SKIP)) {
+      
+      println("Skip clock: " + path)
+      
+      val body: Array[Byte] = Array()
+      val headers: List[NameValuePair] = List()
+      val wrd = new WebResponseData(body, 200, "OK", headers.asJava)
+      return new WebResponse(wrd, webRequest, 10)
+    }
+    
+    return super.getResponse(webRequest);
+  }
+}
