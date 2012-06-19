c89f948af337a9432e023d1d84b37a0437526419
Damien Lecan <dev@dlecan.com>
2012-06-19 13:58:19 +0200

    Formating

--- project-code/plugin/src/main/scala/Play2WarCommands.scala
+++ project-code/plugin/src/main/scala/Play2WarCommands.scala
@@ -19,77 +19,78 @@ import java.io.{ File, ByteArrayInputStream }
 
 trait Play2WarCommands extends sbt.PlayCommands with sbt.PlayReloader {
 
-  val warTask = (baseDirectory, playPackageEverything, dependencyClasspath in Runtime, target, normalizedName, version, streams) map { (root, packaged, dependencies, target, id, version, s) =>
+  val warTask = (baseDirectory, playPackageEverything, dependencyClasspath in Runtime, target, normalizedName, version, streams) map {
+    (root, packaged, dependencies, target, id, version, s) =>
 
-    import sbt.NameFilter._
+      import sbt.NameFilter._
 
-    val warDir = target
-    val packageName = id + "-" + version
-    val war = warDir / (packageName + ".war")
+      val warDir = target
+      val packageName = id + "-" + version
+      val war = warDir / (packageName + ".war")
 
-    s.log.info("Packaging " + war.getCanonicalPath + " ...")
+      s.log.info("Packaging " + war.getCanonicalPath + " ...")
 
-    IO.createDirectory(warDir)
+      IO.createDirectory(warDir)
 
-    val libs = {
-      dependencies.filterNot(_.data.name.contains("servlet")).filter(_.data.ext == "jar").map { dependency =>
-        val filename = for {
-          module <- dependency.metadata.get(AttributeKey[ModuleID]("module-id"))
-          artifact <- dependency.metadata.get(AttributeKey[Artifact]("artifact"))
-        } yield {
-          // groupId.artifactId-version[-classifier].extension
-          module.organization + "." + module.name + "-" + module.revision + artifact.classifier.map("-" + _).getOrElse("") + "." + artifact.extension
-        }
-        val path = ("WEB-INF/lib/" + filename.getOrElse(dependency.data.getName))
-        dependency.data -> path
-      } ++ packaged.map(jar => jar -> ("WEB-INF/lib/" + jar.getName))
-    }
+      val libs = {
+        dependencies.filterNot(_.data.name.contains("servlet")).filter(_.data.ext == "jar").map { dependency =>
+          val filename = for {
+            module <- dependency.metadata.get(AttributeKey[ModuleID]("module-id"))
+            artifact <- dependency.metadata.get(AttributeKey[Artifact]("artifact"))
+          } yield {
+            // groupId.artifactId-version[-classifier].extension
+            module.organization + "." + module.name + "-" + module.revision + artifact.classifier.map("-" + _).getOrElse("") + "." + artifact.extension
+          }
+          val path = ("WEB-INF/lib/" + filename.getOrElse(dependency.data.getName))
+          dependency.data -> path
+        } ++ packaged.map(jar => jar -> ("WEB-INF/lib/" + jar.getName))
+      }
 
-    val webxml = warDir / "web.xml"
+      val webxml = warDir / "web.xml"
 
-    //    if (!webxml.exists) {
-    //      IO.write(webxml,
-    //        """<?xml version="1.0" ?>
-    //<web-app xmlns="http://java.sun.com/xml/ns/j2ee"
-    //        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-    //        xsi:schemaLocation="http://java.sun.com/xml/ns/javaee http://java.sun.com/xml/ns/javaee/web-app_3_0.xsd"
-    //        version="3.0">
-    //  
-    //  <display-name>Play! (%APPLICATION_NAME%)</display-name>
-    //  
-    //  <!--context-param>
-    //    <param-name>play.id</param-name>
-    //    <param-value>%PLAY_ID%</param-value>
-    //  </context-param>
-    //  
-    //  <listener>
-    //      <listener-class>play.core.server.servlet.PlayServletWrapper</listener-class>
-    //  </listener>
-    //  
-    //  <servlet>
-    //    <servlet-name>play</servlet-name>
-    //    <servlet-class>play.core.server.servlet.PlayServletWrapper</servlet-class>	
-    //  </servlet>
-    //    	    
-    //  <servlet-mapping>
-    //    <servlet-name>play</servlet-name>
-    //    <url-pattern>/</url-pattern>
-    //  </servlet-mapping-->
-    //
-    //</web-app>
-    //""" /* */ )
-    //    }
-    //
-    //    val webxmlFile = Seq(webxml -> ("WEB-INF/web.xml"))
+      //    if (!webxml.exists) {
+      //      IO.write(webxml,
+      //        """<?xml version="1.0" ?>
+      //<web-app xmlns="http://java.sun.com/xml/ns/j2ee"
+      //        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      //        xsi:schemaLocation="http://java.sun.com/xml/ns/javaee http://java.sun.com/xml/ns/javaee/web-app_3_0.xsd"
+      //        version="3.0">
+      //  
+      //  <display-name>Play! (%APPLICATION_NAME%)</display-name>
+      //  
+      //  <!--context-param>
+      //    <param-name>play.id</param-name>
+      //    <param-value>%PLAY_ID%</param-value>
+      //  </context-param>
+      //  
+      //  <listener>
+      //      <listener-class>play.core.server.servlet.PlayServletWrapper</listener-class>
+      //  </listener>
+      //  
+      //  <servlet>
+      //    <servlet-name>play</servlet-name>
+      //    <servlet-class>play.core.server.servlet.PlayServletWrapper</servlet-class>	
+      //  </servlet>
+      //    	    
+      //  <servlet-mapping>
+      //    <servlet-name>play</servlet-name>
+      //    <url-pattern>/</url-pattern>
+      //  </servlet-mapping-->
+      //
+      //</web-app>
+      //""" /* */ )
+      //    }
+      //
+      //    val webxmlFile = Seq(webxml -> ("WEB-INF/web.xml"))
 
-    val manifest = new Manifest(
-      new ByteArrayInputStream((
-        "Manifest-Version: 1.0\n").getBytes))
+      val manifest = new Manifest(
+        new ByteArrayInputStream((
+          "Manifest-Version: 1.0\n").getBytes))
 
-    IO.jar(libs /*++ webxmlFile*/ , war, manifest)
+      IO.jar(libs /*++ webxmlFile*/ , war, manifest)
 
-    s.log.info("Done packaging.")
+      s.log.info("Done packaging.")
 
-    war
+      war
   }
 }
