c811f03f2934505874049d1bcf27719a2be696f9
Damien Lecan <dev@dlecan.com>
2012-11-01 20:36:23 +0100

    Begin implementation of Tomcat websocket with Play server

--- project-code/websocket/tomcat/src/main/scala/TomcatWebSocket.scala
+++ project-code/websocket/tomcat/src/main/scala/TomcatWebSocket.scala
@@ -2,16 +2,23 @@ package play.core.server.websocket.tomcat
 
 import java.nio.ByteBuffer
 import java.nio.CharBuffer
-
-import javax.servlet.http.HttpServletRequest;
-
+import javax.servlet.http.HttpServletRequest
 import org.apache.catalina.websocket.MessageInbound
-import org.apache.catalina.websocket.StreamInbound;
+import org.apache.catalina.websocket.StreamInbound
 import org.apache.catalina.websocket.WebSocketServlet
+import javax.servlet.annotation.WebServlet
+import play.core.server.servlet30.Play2Servlet30RequestHandler
+import play.core.server.servlet.Play2WarServer
 
+@WebServlet(name = "TomcatWebsocket", urlPatterns = Array { "/websocket" }, asyncSupported = true)
 class P2WTomcatWebSocketServlet extends WebSocketServlet {
 
   override def createWebSocketInbound(subProtocol: String, request: HttpServletRequest) = {
+
+    val requestHandler = new Play2Servlet30RequestHandler(request)
+    
+//    Play2WarServer.handleRequest(requestHandler)
+
     new P2WTomcatMessageInbound;
   }
 
