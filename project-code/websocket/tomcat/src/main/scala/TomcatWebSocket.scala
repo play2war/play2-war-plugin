5b94aa3d7737204e51fb23f0954759fdde88873f
Damien Lecan <dev@dlecan.com>
2012-11-15 22:32:30 +0100

    Adapt servlet context to test case

--- project-code/websocket/tomcat/src/main/scala/TomcatWebSocket.scala
+++ project-code/websocket/tomcat/src/main/scala/TomcatWebSocket.scala
@@ -7,7 +7,7 @@ import javax.servlet.annotation.WebServlet
 import javax.servlet.http.HttpServletRequest
 import play.core.server.servlet.Play2WarServer
 
-@WebServlet(name = "TomcatWebsocket", urlPatterns = Array { "/websocket" }, asyncSupported = true)
+@WebServlet(name = "TomcatWebsocket", urlPatterns = Array { "/echoMessage" }, asyncSupported = true)
 class P2WTomcatWebSocketServlet extends WebSocketServlet {
 
   override def createWebSocketInbound(subProtocol: String, request: HttpServletRequest) = {
