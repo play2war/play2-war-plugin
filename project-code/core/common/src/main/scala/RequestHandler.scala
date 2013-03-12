83363a686acfa4879a3920104927008bf3a8bf29
Damien Lecan <dev@dlecan.com>
2013-03-12 18:19:19 +0100

    WIP on Sync: respons handling without handleAction method

--- project-code/core/common/src/main/scala/RequestHandler.scala
+++ project-code/core/common/src/main/scala/RequestHandler.scala
@@ -3,31 +3,19 @@ package play.core.server.servlet
 import javax.servlet.http.{ Cookie => ServletCookie }
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
-import play.api.mvc.Cookies
-import play.api.mvc.Headers
+import play.api._
+import play.api.mvc._
 import java.util.concurrent.atomic.AtomicBoolean
-import play.api.mvc.SimpleResult
-import play.api.mvc.ChunkedResult
-import play.api.mvc.RequestHeader
-import play.api.mvc.AsyncResult
-import play.api.mvc.EssentialAction
 import play.api.libs.iteratee.Enumerator
 import play.api.Logger
-import play.api.mvc.WebSocket
-import play.api.mvc.Results
-import play.api.mvc.Response
-import play.api.mvc.Result
-import play.api.mvc.Action
 import play.api.libs.concurrent._
 import play.api.libs.iteratee._
 import scala.concurrent.Future
-import play.api.mvc.ResponseHeader
-import play.api.mvc.Request
 import play.api.http.HeaderNames.CONTENT_LENGTH
 import play.api.http.HeaderNames.X_FORWARDED_FOR
 import java.io.ByteArrayOutputStream
 import java.net.URLDecoder
-import play.api.mvc.RequestTaggingHandler
+import play.api.http.HeaderNames
 
 trait RequestHandler {
 
@@ -359,13 +347,13 @@ abstract class Play2GenericServletRequestHandler(val servletRequest: HttpServlet
                   case (_, in) => Done((), in)
                 }
                 Iteratee.flatten(Promise.pure()
-                    .extend1 {
-                      case Redeemed(_) => if (!hasError.get) Cont(step) else Done((), Input.Empty: Input[r.BODY_CONTENT])
-                      case Thrown(ex) =>
-                        Logger("play").debug(ex.toString)
-                        hasError.set(true)
-                        throw ex
-                    })
+                  .extend1 {
+                    case Redeemed(_) => if (!hasError.get) Cont(step) else Done((), Input.Empty: Input[r.BODY_CONTENT])
+                    case Thrown(ex) =>
+                      Logger("play").debug(ex.toString)
+                      hasError.set(true)
+                      throw ex
+                  })
               }
 
               chunks apply bodyIteratee.map { _ =>
@@ -389,15 +377,36 @@ abstract class Play2GenericServletRequestHandler(val servletRequest: HttpServlet
 
     }
 
+    def cleanFlashCookie(r: PlainResult): Result = {
+      val header = r.header
+
+      val flashCookie = {
+        header.headers.get(HeaderNames.SET_COOKIE)
+          .map(Cookies.decode(_))
+          .flatMap(_.find(_.name == Flash.COOKIE_NAME)).orElse {
+            Option(requestHeader.flash).filterNot(_.isEmpty).map { _ =>
+              Flash.discard.toCookie
+            }
+          }
+      }
+
+      flashCookie.map { newCookie =>
+        r.withHeaders(HeaderNames.SET_COOKIE -> Cookies.merge(header.headers.get(HeaderNames.SET_COOKIE).getOrElse(""), Seq(newCookie)))
+      }.getOrElse(r)
+    }
+
     handler match {
 
       //execute normal action
-      case Right((action: EssentialAction, app)) => {
+      case Right((action: EssentialAction, app)) =>
         val a = EssentialAction { rh =>
-          Iteratee.flatten(action(rh).unflatten.extend1 {
-            case Redeemed(it) => it.it
-            case Thrown(e) => Done(app.handleError(requestHeader, e), Input.Empty)
-          })
+          Iteratee.flatten(action(rh).unflatten. /*map {
+            case r: PlainResult => cleanFlashCookie(r)
+            case a: AsyncResult => a.transform(cleanFlashCookie)
+          }.*/ extend1 {
+              case Redeemed(it) => it.it
+              case Thrown(e) => Done(app.handleError(requestHeader, e), Input.Empty)
+            })
         }
 
         Logger("play").trace("Serving this request with: " + a)
@@ -435,18 +444,25 @@ abstract class Play2GenericServletRequestHandler(val servletRequest: HttpServlet
             response.handle(Results.InternalServerError)
           }
         }
-      }
 
-      //handle websocket action
-      case Right((ws @ WebSocket(f), app)) => {
-        Logger("play").error("Impossible to serve Web Socket request:" + ws)
-        response.handle(Results.InternalServerError)
-      }
+      //handle bad websocket request
+      case Right((WebSocket(_), app)) =>
+        Logger("play").trace("Bad websocket request")
+        val a = EssentialAction(_ => Done(Results.BadRequest, Input.Empty))
+        handleAction(a, Some(app))
 
-      case unexpected => {
+      case Left(e) =>
+        Logger("play").trace("No handler, got direct result: " + e)
+        val a = EssentialAction(_ => Done(e, Input.Empty))
+        handleAction(a, None)
+
+      case unexpected =>
         Logger("play").error("Oops, unexpected message received in Play server (please report this problem): " + unexpected)
         response.handle(Results.InternalServerError)
-      }
+    }
+    
+    def handleAction(a:EssentialAction,app:Option[Application]){
+      
     }
 
     onFinishService()
