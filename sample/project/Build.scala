e2ed049d4a461f3c0218c1fed9867b149446cb07
Damien Lecan <dev@dlecan.com>
2013-05-30 12:41:57 +0200

    Fix compilation error

--- sample/project/Build.scala
+++ sample/project/Build.scala
@@ -12,7 +12,7 @@ object ApplicationBuild extends Build {
   lazy val commonSettings = Defaults.defaultSettings ++ Seq(
     //      resolvers += "Local Repository" at "http://localhost:8090/publish",
     // resolvers += Resolver.file("Local Ivy Repository", file(Path.userHome.absolutePath + "/.ivy2/local"))(Resolver.ivyStylePatterns),
-    resolvers += Resolver.defaultLocal
+    resolvers += Resolver.defaultLocal,
     resolvers += Resolver.sonatypeRepo("snapshots"),
 
     EclipseKeys.withSource := true,
