7f8f43b8f75db8aac8c62e635b12e3236edd000b
Piotr Wyczesany <piotr.wyczesany@xtrf.eu>
2019-09-07 12:18:52 +0200

    WIP #321 TODOs cleaned up

--- project-code/core/servlet31/src/main/scala/play/core/server/servlet31/Play2Servlet31.scala
+++ project-code/core/servlet31/src/main/scala/play/core/server/servlet31/Play2Servlet31.scala
@@ -1,15 +1,15 @@
 package play.core.server.servlet31
 
 import javax.servlet.annotation.{WebListener, WebServlet}
-import javax.servlet.http.{HttpServletResponse, HttpServletRequest}
-import play.api.{Configuration, Logger}
+import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
+import play.api.Logger
 import play.core.server.servlet.{GenericPlay2Servlet, Play2WarServer}
 
 
 object Play2Servlet {
   private[this] val configuration = Play2WarServer.configuration
-  val asyncTimeout = configuration.getInt("servlet31.asynctimeout").getOrElse(-1)
-  val internalUploadBufferSide = configuration.getInt("servlet31.upload.internalbuffersize").getOrElse(1024 * 8)
+  val asyncTimeout: Int = configuration.getOptional[Int]("servlet31.asynctimeout").getOrElse(-1)
+  val internalUploadBufferSide: Int = configuration.getOptional[Int]("servlet31.upload.internalbuffersize").getOrElse(1024 * 8)
   Logger("play").debug("Async timeout for HTTP requests: " + asyncTimeout + " ms")
 }
 
